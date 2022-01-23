e28a07840a0d1d1819934e429f9e414fb73e3526
Nikhilesh Manurkar <nikhilesh.prefetch@gmail.com>
2022-01-24 02:45:29 +0530

    Project Start
    Starting Files for Initial Install

---
+++ src/Zohobooks.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace beingnikhilesh\zohobooks;
+
+use beingnikhilesh\zohobooks\Company;
+
+class Zohobooks {
+
+    function __construct() {
+        parent::__construct();
+    }
+
+}
